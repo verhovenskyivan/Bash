020a4e8d780aa9e11350dc222cff4276a74c5e29
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-06-25 17:30:07 +0300

    Create low disk space.sh

---
+++ low disk space.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+THRESHOLD=90
+if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
+mail -s 'Заканчивается дисковое пространство' -r admin@example.com << EOF
+В вашем корневом разделе сервера server1, осталось слишком мало дискового пространства. Используется: $CURRENT%
+EOF
+fi
